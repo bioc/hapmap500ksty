6c71b88f9b61d88f0b1a3bee1e3dd0f4c92774f0
Benilton Carvalho <bcarvalh@jhsph.edu>
2012-09-24 15:41:48 +0000

    Fixing startup messages
    git-svn-id: file:///home/git/bioc-data.hedgehog.fhcrc.org/trunk/experiment/pkgs/hapmap500ksty@2160 db2202da-8704-0410-b924-b125b3cd84ef

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,5 @@
-.onLoad <- function(libname, pkgname) {
-  message(paste("/-------------------------------------------\\",
+.onAttach <- function(libname, pkgname) {
+  packageStartupMessage(paste("/-------------------------------------------\\",
                 "|         SAMPLE HAPMAP 500K STY            |",
                 "|-------------------------------------------|",
                 "| Data obtained from http://www.hapmap.org  |",
